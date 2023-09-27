6e2508fc17249894f075b2e45ba1684df6c0593a
felixleandr <felixleander2108@gmail.com>
2023-09-27 16:01:46 +0700

    coba2

---
+++ migrations/20230927085310-create-favorite.js
@@ -0,0 +1,39 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up(queryInterface, Sequelize) {
+    return queryInterface.createTable('Favorites', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      ProfileId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Profiles",
+          key: 'id'
+        }
+      },
+      BookId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Books",
+          key: 'id'
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Favorites');
+  }
+};
