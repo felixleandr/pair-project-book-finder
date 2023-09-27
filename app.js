4986f0a86fab84d23887dd80a889ca7f973a96de
adhyapandu28 <adhyapandu29@gmail.com>
2023-09-28 04:22:12 +0700

    pengerjaan login register nodemailer, session & bcryptjs

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
 const express = require('express')
+const session = require('express-session');
 const app = express()
 const port = 7000
+require('dotenv').config();
 const Controller = require('./controllers/controllers')
+
+app.use(session({
+  secret: 'Hactiv8',  
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }   
+}));
+
+function isAuthenticated(req, res, next) {
+  if (req.session && req.session.isAuthenticated) {
+    next(); 
+  } else {
+    res.redirect('/login');
+  }
+}
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended: true}))
 
-app.get('/', Controller.home)
+app.get('/register', Controller.registerPage);
+app.post('/register', Controller.logicRegister);
+app.get('/login',Controller.loginPage );
+app.post('/login',Controller.loginLogic );
+
+
+app.get('/',isAuthenticated, Controller.home) // isauthenticated ini buat session nya jd klo mau masuk 
+//halaman ini harus login dulu, yang berari kita harus bikin button logut juga nanti, ini terapinnya nanti aja klo semua
+//pagenya udah selesai biar ga ribet harus login
 app.get('/books', Controller.listBooks)
 app.get('/books/add', Controller.showFormAddBook)
 app.post('/books/add', Controller.addBook)
--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,7 +1,57 @@
-const {Book, Profile, Publisher} = require('../models')
+const {User,Book, Profile, Publisher} = require('../models')
 const {Op} = require('sequelize')
+const { sendRegistrationEmail } = require('../mailer');
 
 class Controller {
+    static registerPage(req, res){
+        res.render('registerPage');
+      }
+    static loginPage(req,res){
+        res.render('loginPage'); 
+    }
+    static logicRegister(req, res){
+        const { username, email, password , role} = req.body;
+        console.log(req.body)
+        User.create({
+            username: username,
+            email: email,
+            password: password ,
+            role: role
+        })
+        .then(user => {
+            sendRegistrationEmail(user.email);
+            res.redirect('/login');
+        })
+        .catch(err => {
+            if (err.name === 'SequelizeUniqueConstraintError') {
+                res.render('registerPage', { error: 'Username atau email sudah ada. Silakan pilih yang lain.' });
+            }  else {
+                res.render('registerPage', { error: 'Terjadi kesalahan saat pendaftaran.' });
+                console.error(err);
+            }
+        });
+      }
+    static loginLogic(req, res){
+        const { username, password } = req.body;
+        User.findOne({ where: { username } })
+        .then(user => {
+            if (!user) {
+                throw new Error('User not found');
+            }
+            return user.isValidPassword(password);
+        })
+        .then(isMatch => {
+            if (!isMatch) {
+                throw new Error('Password is incorrect');
+            }
+             req.session.isAuthenticated = true;
+            res.redirect('/');
+        })
+        .catch(err => {
+            res.render('loginPage', { error: err.message });
+            // console.log(err)
+        });
+      }
     static home(req, res){
         res.render('home')
     }
